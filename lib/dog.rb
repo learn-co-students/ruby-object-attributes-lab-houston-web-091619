1d9e7f35804cefa42ff70d54397a948bd3ff6fcd
Patrick Alexander <patrick_alexander@mail.com>
2019-09-17 16:38:34 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+    
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed 
+        @breed
+    end
+
+    
+
+    # attr_accesor :name, :breed
+
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end
+end
